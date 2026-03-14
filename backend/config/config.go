9ebf1cb44cefa26d6c5dd3c362dc7f1bfac71ebd
icelain <icelain@ice.computer>
2026-03-14 18:33:34 +0530

    add flag for rebuild control

--- backend/config/config.go
+++ backend/config/config.go
@@ -4,10 +4,12 @@ const (
 	DefaultAddr     = "127.0.0.1"
 	DefaultPort     = 8080
 	DefaultLogLevel = "info"
+	DefaultRebuildImage = false
 )
 
 var (
 	Addr     = DefaultAddr
 	Port     = DefaultPort
 	LogLevel = DefaultLogLevel
+	RebuildImage = DefaultRebuildImage
 )
